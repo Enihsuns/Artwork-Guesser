e861b444d752b86e4d8f2663cec599d71fe58cd9
Enihsuns <enihsunsyxy@gmail.com>
2019-09-20 09:29:39 -0400

    Initial Frontend: React and Material UI.

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "@material-ui/core/Button";
+import Album from "./Album";
+
+ReactDOM.render(
+    <div>
+        <Album />
+    </div>, 
+    document.querySelector("#app")
+);
