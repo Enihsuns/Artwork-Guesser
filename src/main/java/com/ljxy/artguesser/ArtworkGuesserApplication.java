89c65cb225129f18e660c4c05da97a8df401d526
Enihsuns <enihsunsyxy@gmail.com>
2019-09-20 08:28:45 -0400

    Initial Commit: Basic Spring Boot architecture.

---
+++ src/main/java/com/ljxy/artguesser/ArtworkGuesserApplication.java
@@ -0,0 +1,13 @@
+package com.ljxy.artguesser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtworkGuesserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtworkGuesserApplication.class, args);
+    }
+
+}
